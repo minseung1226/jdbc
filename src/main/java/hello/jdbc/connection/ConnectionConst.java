06f4561534f8d6bc8cc6ad490286486626803ba3
minseung1226 <minseung1226@naver.com>
2023-01-08 19:26:10 +0900

    JDBC 이해 -JDBC이해

---
+++ src/main/java/hello/jdbc/connection/ConnectionConst.java
@@ -0,0 +1,7 @@
+package hello.jdbc.connection;
+
+public abstract class ConnectionConst {
+    public final static String URL = "jdbc:h2:tcp://localhost/~/test";
+    public final static String USERNAME="sa";
+    public final static String PASSWORD="";
+}
